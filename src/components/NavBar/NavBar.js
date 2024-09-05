c99272b1e1605cf840f9d0ce17277eb3cd5bf13d
Artjom <anikok@taltech.ee>
2024-09-05 19:35:07 +0300

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,23 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import logo from '../../assets/logo.png'
 import contactImg from '../../assets/contact.png'
+import menu from '../../assets/menu.png'
 import {Link} from 'react-scroll';
 
-const navbar = () => {
+const Navbar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav className="navbar">
         <img src={logo} alt='Logo' className='logo'></img>
         <div className="desktopMenu">
-          <Link className="desktopMenuListItem">Home</Link>
-          <Link className="desktopMenuListItem">About</Link>
-          <Link className="desktopMenuListItem">Portfolio</Link>
-          <Link className="desktopMenuListItem">Clients</Link>
+          <Link className="desktopMenuListItem"
+                activeClass='active' 
+                to='intro' 
+                spy={true} 
+                smooth={true} 
+                offset={-100} 
+                duration={500}>Home</Link>
+
+          <Link className="desktopMenuListItem" 
+                activeClass='active' 
+                to='skills' 
+                spy={true} 
+                smooth={true} 
+                offset={-1} 
+                duration={500}>About</Link>
+
+          <Link className="desktopMenuListItem"
+                activeClass='active' 
+                to='' 
+                spy={true} 
+                smooth={true} 
+                offset={-50} 
+                duration={500}>Portfolio</Link>
+
+          <Link className="desktopMenuListItem"
+                activeClass='active' 
+                to='' 
+                spy={true} 
+                smooth={true} 
+                offset={-50} 
+                duration={500}>Clients</Link>
+
         </div>
-        <button className="desktopMenuBtn">
+        <button className="desktopMenuBtn" onClick={() => {
+          document.getElementById('skills').scrollIntoView({behavior: 'smooth'});
+        }}>
             <img src={contactImg} alt="Contact Me" className="desktopMenuImg" />Contact Me</button>
+
+        <img src={menu} alt='Menu' className='mobMenu' onClick={() => setShowMenu(!showMenu)}></img>
+        <div className="navMenu" style={{display: showMenu ? 'flex':'none'}}>
+
+          <Link className="listItem"
+                onClick={() => setShowMenu(false)}
+                activeClass='active' 
+                to='intro' 
+                spy={true} 
+                smooth={true} 
+                offset={-100}
+                duration={500}>Home</Link>
+
+          <Link className="listItem"
+                onClick={() => setShowMenu(false)}
+                activeClass='active' 
+                to='skills' 
+                spy={true} 
+                smooth={true} 
+                offset={-1} 
+                duration={500}>About</Link>
+
+          <Link className="listItem"
+                onClick={() => setShowMenu(false)}
+                activeClass='active' 
+                to='' 
+                spy={true} 
+                smooth={true} 
+                offset={-50} 
+                duration={500}>Portfolio</Link>
+
+          <Link className="listItem"
+                onClick={() => setShowMenu(false)}
+                activeClass='active' 
+                to='' 
+                spy={true} 
+                smooth={true} 
+                offset={-50} 
+                duration={500}>Clients</Link>
+
+        </div>
     </nav>
   )
 }
 
-export default navbar
+export default Navbar
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -14,7 +14,15 @@ const Skills = () => {
                 <img src={UIDesign} alt='UiDesign' className='skillBarImg'></img>
                 <div className='skillBarText'>
                     <h2>UI/UX Design</h2>
-                    <p>This is a demo text, you can write your own content here.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                        deserunt mollit anim id est laborum. "Sed ut perspiciatis unde omnis iste natus error sit 
+                        voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
+                        inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."</p>
                 </div>
             </div>
 
@@ -22,7 +30,15 @@ const Skills = () => {
                 <img src={WebDesign} alt='WebDesign' className='skillBarImg'></img>
                 <div className='skillBarText'>
                     <h2>Website Design</h2>
-                    <p>This is a demo text, you can write your own content here.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                        deserunt mollit anim id est laborum. "Sed ut perspiciatis unde omnis iste natus error sit 
+                        voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+                        veritatis et quasi architecto beatae vitae dicta sunt explicabo."</p>
                 </div>
             </div>
 
@@ -30,7 +46,15 @@ const Skills = () => {
                 <img src={AppDesign} alt='AppDesign' className='skillBarImg'></img>
                 <div className='skillBarText'>
                     <h2>App Design</h2>
-                    <p>This is a demo text, you can write your own content here.</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                        in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                        deserunt mollit anim id est laborum. "Sed ut perspiciatis unde omnis iste natus error 
+                        sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo 
+                        inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."</p>
                 </div>
             </div>
         </div>
