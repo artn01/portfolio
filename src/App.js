2388b3968a77952a2180f11a18f7cfb4b927cd76
Artjom <anikok@taltech.ee>
2024-09-05 17:59:08 +0300

    about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar/NavBar";
 import Intro from "./components/Intro/Intro";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Intro></Intro>
+      <Skills></Skills>
     </div>
   );
 }
